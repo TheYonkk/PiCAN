3635e596ed8ec22434d8cc1bff3d04146099a034
Dave Yonkers <yonkers4@msu.edu>
2022-04-29 21:32:40 -0400

    Changes to client for automation/services

--- client/build.sh
+++ client/build.sh
@@ -6,17 +6,9 @@ sudo apt-get install g++ -y
 # install cmake
 sudo apt-get install cmake -y
 
-# build and install dbcppp
-mkdir src/external/dbcppp/build
-cd src/external/dbcppp/build
-cmake -DCMAKE_BUILD_TYPE=Release -Dbuild_tests=OFF -Dbuild_examples=OFF ..
-make -j
-sudo make install
-sudo ldconfig
-
 # build the logger application
-mkdir ../../../build   # (PiLogger/logger/src/build)
-cd ../../../build
+mkdir src/build   # (PiCAN/client/src/build)
+cd src/build
 cmake ..
 make
 
